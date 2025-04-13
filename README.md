fca59b21cef7c083680ab2f8a6addb644d4f2493
waladir <ludek@hrbkovi.eu>
2025-04-13 14:19:48 +0200

    1.3.2

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ VOD, živé vysílání, archiv, nahrávky, vytváření playlistu a EPG pro IPT
 <a href="https://www.xbmc-kodi.cz/prispevek-oneplay">Vlákno na fóru XBMC-Kodi.cz</a><br><br>
 </p>
 <p>
+1.3.2 (13.4.2025)<br>
+- u archivu přidání přehrávaní následujícího pořadu<br><br>
+
 1.3.1 (12.4.2025)<br>
 - přidání možnosti nastavení profilového PINu<br>
 - u živého vysílání i archivu lze v kontextovém menu zobrazit seznam dostupných epizod v archivu<br><br>
@@ -19,7 +22,4 @@ VOD, živé vysílání, archiv, nahrávky, vytváření playlistu a EPG pro IPT
 
 1.2.8 (4.4.2025)<br>
 - oprava spouštění HLS-only streamů<br><br>
-
-1.2.7 (3.4.2025)<br>
-- oprava mazání a zobrazení nahrávek<br><br>
 </p>
--- resources/lib/stream.py
+++ resources/lib/stream.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import xbmc
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
@@ -15,49 +16,13 @@ from resources.lib.epg import get_channel_epg
 if len(sys.argv) > 1:
     _handle = int(sys.argv[1])
 
-def play_catchup(id, start_ts, end_ts):
-    start_ts = int(start_ts)
-    end_ts = int(end_ts)    
-    epg = get_channel_epg(channel_id = id, from_ts = start_ts, to_ts = end_ts + 60*60*12)
-    if start_ts in epg:
-        if epg[start_ts]['endts'] > int(time.mktime(datetime.now().timetuple()))-10:
-            play_stream(id, 'start')
-        else:
-            play_stream(epg[start_ts]['id'], 'archive')
-    else:
-        play_stream(id, 'start')
-
-def play_stream(id, mode, nextid = None):
-    addon = xbmcaddon.Addon()
-    session = Session()
+def get_stream_url(post, mode, next = False):
     api = API()
+    session = Session()
     url_dash = None
     url_dash_drm = None
     url_hls = None
-    if mode in ['live', 'start']:
-        channels = Channels()
-        channels_list = channels.get_channels_list('id')
-        channel = channels_list[id]
-        if channel['adult'] == True:
-            if str(addon.getSetting('pin')) == '1621' or len(str(addon.getSetting('pin'))) == 0:
-                pin = xbmcgui.Dialog().numeric(type = 0, heading = 'Zadejte PIN', bHiddenInput = True)
-                if len(str(pin)) != 4:
-                    xbmcgui.Dialog().notification('Oneplay','Nezadaný-nesprávný PIN', xbmcgui.NOTIFICATION_ERROR, 5000)
-                    pin = '1621'
-            else:
-                pin = str(addon.getSetting('pin'))
-            post = {"authorization":[{"schema":"PinRequestAuthorization","pin":pin,"type":"parental"}],"payload":{"criteria":{"schema":"ContentCriteria","contentId":"channel." + id},"startMode":"live"},"playbackCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","subtitle":{"formats":["vtt"],"locations":["InstreamTrackLocation","ExternalTrackLocation"]},"liveSpecificCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","multipleAudio":False}}}
-        else:
-            post = {"payload":{"criteria":{"schema":"ContentCriteria","contentId":"channel." + id},"startMode":mode},"playbackCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","subtitle":{"formats":["vtt"],"locations":["InstreamTrackLocation","ExternalTrackLocation"]},"liveSpecificCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","multipleAudio":False}}}
-    else:
-        post = {"payload":{"contentId":id}}
-        data = api.call_api(url = 'https://http.cms.jyxo.cz/api/v3/page.content.display', data = post, session = session)
-        if 'err' not in data:
-            for block in data['layout']['blocks']:            
-                if block['schema'] == 'ContentHeaderBlock':
-                    if 'mainAction' in block and 'action' in block['mainAction'] and 'criteria' in block['mainAction']['action']['params']['payload'] and 'contentId' in block['mainAction']['action']['params']['payload']['criteria']:
-                        id = block['mainAction']['action']['params']['payload']['criteria']['contentId']
-        post = {"payload":{"criteria":{"schema":"ContentCriteria","contentId":id}},"playbackCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","subtitle":{"formats":["vtt"],"locations":["InstreamTrackLocation","ExternalTrackLocation"]},"liveSpecificCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","multipleAudio":False}}}
+    drm = None
     data = api.call_api(url = 'https://http.cms.jyxo.cz/api/v3/content.play', data = post, session = session)
     if 'err' in data or 'media' not in data:
         if len(data['err']) > 0:
@@ -65,7 +30,7 @@ def play_stream(id, mode, nextid = None):
         else:
             xbmcgui.Dialog().notification('Oneplay', 'Problém při přehrání', xbmcgui.NOTIFICATION_ERROR, 5000)                    
     else:
-        if 'liveControl' in data['playerControl'] and 'mosaic' in data['playerControl']['liveControl']:
+        if 'liveControl' in data['playerControl'] and 'mosaic' in data['playerControl']['liveControl'] and next == False:
             md_titles = []
             md_ids = []
             for item in data['playerControl']['liveControl']['mosaic']['items']:
@@ -96,43 +61,127 @@ def play_stream(id, mode, nextid = None):
             if asset['protocol'] == 'hls':
                 if 'drm' not in asset:
                     url_hls = asset['src']
-        if addon.getSetting('prefer_hls') == 'true' and url_hls is not None:
-            list_item = xbmcgui.ListItem(path = url_hls)
-            list_item.setProperty('inputstream', 'inputstream.adaptive')
-            list_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
-            list_item.setContentLookup(False)       
-            xbmcplugin.setResolvedUrl(_handle, True, list_item)
-        elif url_dash is not None:
-            list_item = xbmcgui.ListItem(path = url_dash)
-            list_item.setProperty('inputstream', 'inputstream.adaptive')
-            list_item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
-            list_item.setMimeType('application/dash+xml')
-            list_item.setContentLookup(False)       
-            xbmcplugin.setResolvedUrl(_handle, True, list_item)
-        elif url_dash_drm is not None:
-            list_item = xbmcgui.ListItem(path = url_dash_drm)
-            list_item.setProperty('inputstream', 'inputstream.adaptive')
-            list_item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
-            if drm is not None:
+    return url_hls, url_dash, url_dash_drm, drm
+
+def play_catchup(id, start_ts, end_ts):
+    start_ts = int(start_ts)
+    end_ts = int(end_ts)    
+    epg = get_channel_epg(channel_id = id, from_ts = start_ts, to_ts = end_ts + 60*60*12)
+    if start_ts in epg:
+        if epg[start_ts]['endts'] > int(time.mktime(datetime.now().timetuple()))-10:
+            play_stream(id, 'start')
+        else:
+            play_stream(epg[start_ts]['id'], 'archive')
+    else:
+        play_stream(id, 'start')
+
+def play_stream(id, mode, nextid = None):
+    addon = xbmcaddon.Addon()
+    api = API()
+    session = Session()
+    if mode in ['live', 'start']:
+        channels = Channels()
+        channels_list = channels.get_channels_list('id')
+        channel = channels_list[id]
+        if channel['adult'] == True:
+            if str(addon.getSetting('pin')) == '1621' or len(str(addon.getSetting('pin'))) == 0:
+                pin = xbmcgui.Dialog().numeric(type = 0, heading = 'Zadejte PIN', bHiddenInput = True)
+                if len(str(pin)) != 4:
+                    xbmcgui.Dialog().notification('Oneplay','Nezadaný-nesprávný PIN', xbmcgui.NOTIFICATION_ERROR, 5000)
+                    pin = '1621'
+            else:
+                pin = str(addon.getSetting('pin'))
+            post = {"authorization":[{"schema":"PinRequestAuthorization","pin":pin,"type":"parental"}],"payload":{"criteria":{"schema":"ContentCriteria","contentId":"channel." + id},"startMode":"live"},"playbackCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","subtitle":{"formats":["vtt"],"locations":["InstreamTrackLocation","ExternalTrackLocation"]},"liveSpecificCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","multipleAudio":False}}}
+        else:
+            post = {"payload":{"criteria":{"schema":"ContentCriteria","contentId":"channel." + id},"startMode":mode},"playbackCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","subtitle":{"formats":["vtt"],"locations":["InstreamTrackLocation","ExternalTrackLocation"]},"liveSpecificCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","multipleAudio":False}}}
+    else:
+        post = {"payload":{"contentId":id}}
+        data = api.call_api(url = 'https://http.cms.jyxo.cz/api/v3/page.content.display', data = post, session = session)
+        if 'err' not in data:
+            for block in data['layout']['blocks']:            
+                if block['schema'] == 'ContentHeaderBlock':
+                    if 'mainAction' in block and 'action' in block['mainAction'] and 'criteria' in block['mainAction']['action']['params']['payload'] and 'contentId' in block['mainAction']['action']['params']['payload']['criteria']:
+                        id = block['mainAction']['action']['params']['payload']['criteria']['contentId']
+        if nextid is not None:
+            post = {"payload":{"criteria":{"schema":"ContentCriteria","contentId":nextid}},"playbackCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","subtitle":{"formats":["vtt"],"locations":["InstreamTrackLocation","ExternalTrackLocation"]},"liveSpecificCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","multipleAudio":False}}}
+            next_url_hls, next_url_dash, next_url_dash_drm, next_drm = get_stream_url(post, mode, True)
+        post = {"payload":{"criteria":{"schema":"ContentCriteria","contentId":id}},"playbackCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","subtitle":{"formats":["vtt"],"locations":["InstreamTrackLocation","ExternalTrackLocation"]},"liveSpecificCapabilities":{"protocols":["dash","hls"],"drm":["widevine","fairplay"],"altTransfer":"Unicast","multipleAudio":False}}}
+
+    url_hls, url_dash, url_dash_drm, drm = get_stream_url(post, mode)
+    if addon.getSetting('prefer_hls') == 'true' and url_hls is not None:
+        list_item = xbmcgui.ListItem(path = url_hls)
+        list_item.setProperty('inputstream', 'inputstream.adaptive')
+        list_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+        list_item.setContentLookup(False)     
+        if nextid is not None:
+            next_list_item = xbmcgui.ListItem(path = next_url_hls)
+            next_list_item.setProperty('inputstream', 'inputstream.adaptive')
+            next_list_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+            next_list_item.setContentLookup(False)       
+            playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+            playlist.add(next_url_hls, next_list_item)
+        xbmcplugin.setResolvedUrl(_handle, True, list_item)
+    elif url_dash is not None:
+        list_item = xbmcgui.ListItem(path = url_dash)
+        list_item.setProperty('inputstream', 'inputstream.adaptive')
+        list_item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
+        list_item.setMimeType('application/dash+xml')
+        list_item.setContentLookup(False)       
+        if nextid is not None:
+            next_list_item = xbmcgui.ListItem(path = next_url_dash)
+            next_list_item.setProperty('inputstream', 'inputstream.adaptive')
+            next_list_item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
+            next_list_item.setMimeType('application/dash+xml')
+            next_list_item.setContentLookup(False)       
+            playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+            playlist.add(next_url_dash, next_list_item)
+        xbmcplugin.setResolvedUrl(_handle, True, list_item)
+    elif url_dash_drm is not None:
+        list_item = xbmcgui.ListItem(path = url_dash_drm)
+        list_item.setProperty('inputstream', 'inputstream.adaptive')
+        list_item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
+        if drm is not None:
+            # from inputstreamhelper import Helper # type: ignore
+            # is_helper = Helper('mpd', drm = 'com.widevine.alpha')
+            # if is_helper.check_inputstream():            
+            list_item.setProperty('inputstream.adaptive.license_type', 'com.widevine.alpha')
+            from urllib.parse import urlencode
+            list_item.setProperty('inputstream.adaptive.license_key', drm['licenceUrl'] + '|' + urlencode({'x-axdrm-message' : drm['token']}) + '|R{SSM}|')                
+        list_item.setMimeType('application/dash+xml')
+        list_item.setContentLookup(False)       
+        if nextid is not None:
+            next_list_item = xbmcgui.ListItem(path = next_url_dash_drm)
+            next_list_item.setProperty('inputstream', 'inputstream.adaptive')
+            next_list_item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
+            if next_drm is not None:
                 # from inputstreamhelper import Helper # type: ignore
                 # is_helper = Helper('mpd', drm = 'com.widevine.alpha')
                 # if is_helper.check_inputstream():            
                 list_item.setProperty('inputstream.adaptive.license_type', 'com.widevine.alpha')
                 from urllib.parse import urlencode
                 list_item.setProperty('inputstream.adaptive.license_key', drm['licenceUrl'] + '|' + urlencode({'x-axdrm-message' : drm['token']}) + '|R{SSM}|')                
-            list_item.setMimeType('application/dash+xml')
+            next_list_item.setMimeType('application/dash+xml')
+            next_list_item.setContentLookup(False)       
+            playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+            playlist.add(next_url_dash, next_list_item)
+        xbmcplugin.setResolvedUrl(_handle, True, list_item)
+    elif url_hls is not None:
+        if mode == 'start':
+            if 'playerControl' in data and 'liveControl' in data['playerControl'] and 'channelId' in data['playerControl']['liveControl']:
+                play_stream(data['playerControl']['liveControl']['channelId'].replace('channel.',''), 'live')
+        else:
+            list_item = xbmcgui.ListItem(path = url_hls)
+            list_item.setProperty('inputstream', 'inputstream.adaptive')
+            list_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
             list_item.setContentLookup(False)       
+            if nextid is not None:
+                next_list_item = xbmcgui.ListItem(path = next_url_hls)
+                next_list_item.setProperty('inputstream', 'inputstream.adaptive')
+                next_list_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+                next_list_item.setContentLookup(False)       
+                playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+                playlist.add(next_url_hls, next_list_item)
             xbmcplugin.setResolvedUrl(_handle, True, list_item)
-        elif url_hls is not None:
-            if mode == 'start':
-                if 'playerControl' in data and 'liveControl' in data['playerControl'] and 'channelId' in data['playerControl']['liveControl']:
-                    play_stream(data['playerControl']['liveControl']['channelId'].replace('channel.',''), 'live')
-            else:
-                list_item = xbmcgui.ListItem(path = url_hls)
-                list_item.setProperty('inputstream', 'inputstream.adaptive')
-                list_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
-                list_item = xbmcgui.ListItem(path = url_hls)
-                list_item.setContentLookup(False)       
-                xbmcplugin.setResolvedUrl(_handle, True, list_item)
     else:
         xbmcgui.Dialog().notification('Oneplay','Problém při přehrání', xbmcgui.NOTIFICATION_ERROR, 5000)
+
